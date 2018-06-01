7eb6f8bc43c325aa0a87fc53594937a17cc7a173
Dice <tetradice@gmail.com>
2018-06-02 00:23:26 +0900

    redis

--- server.ts
+++ server.ts
@@ -3,7 +3,9 @@
 import * as express from 'express';
 import * as socketIO from 'socket.io';
 import * as path from 'path';
+import * as redis from 'redis';
 
+const RedisClient = redis.createClient(process.env.REDIS_URL);
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
@@ -18,4 +20,9 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => {
+  let count = RedisClient.INCR('counter', (error, n) => {
+    io.emit('time', n);
+  });
+  
+}, 1000);
