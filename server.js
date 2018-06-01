7f871ff296db1b0c6b0e53567ec73b25785645a1
Dice <tetradice@gmail.com>
2018-06-02 00:26:28 +0900

    redis

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const socketIO = require("socket.io");
 const path = require("path");
+const redis = require("redis");
+const RedisClient = redis.createClient(process.env.REDIS_URL);
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 const server = express()
@@ -13,4 +15,8 @@ io.on('connection', (socket) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
 });
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => {
+    let count = RedisClient.INCR('counter', (error, n) => {
+        io.emit('time', n);
+    });
+}, 1000);
