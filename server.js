bb54d92800442393c1a6e7242300a10c46dced5c
Dice <tetradice@gmail.com>
2018-06-02 09:14:08 +0900

    基本的な卓生成アルゴリズム完成

--- server.js
+++ server.js
@@ -4,19 +4,31 @@ const express = require("express");
 const socketIO = require("socket.io");
 const path = require("path");
 const redis = require("redis");
+const randomstring = require("randomstring");
 const RedisClient = redis.createClient(process.env.REDIS_URL);
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 const server = express()
-    .use((req, res) => res.sendFile(INDEX))
+    .use(express.static('public'))
+    .get('/', (req, res) => res.sendFile(INDEX))
+    .post('/tables.create', (req, res) => {
+    // 新しい卓IDを生成
+    let tableId = randomstring.generate({
+        length: 8,
+        readable: true
+    });
+    // 卓を追加
+    RedisClient.HSET('tables', tableId, JSON.stringify({ created: new Date().toJSON() }));
+    // 卓にアクセスするためのURLを生成
+    let urlBase = req.protocol + '://' + req.hostname;
+    let p1Url = `${urlBase}/t/${tableId}/p1`;
+    let p2Url = `${urlBase}/t/${tableId}/p2`;
+    let watchUrl = `${urlBase}/t/${tableId}/watch`;
+    res.json({ p1Url: p1Url, p2Url: p2Url, watchUrl: watchUrl });
+})
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 const io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
 });
-setInterval(() => {
-    let count = RedisClient.INCR('counter', (error, n) => {
-        io.emit('time', n);
-    });
-}, 1000);
--- server.ts
+++ server.ts
@@ -4,13 +4,33 @@ import * as express from 'express';
 import * as socketIO from 'socket.io';
 import * as path from 'path';
 import * as redis from 'redis';
+import * as randomstring from 'randomstring';
 
 const RedisClient = redis.createClient(process.env.REDIS_URL);
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
+  .use(express.static('public'))
+  .get('/', (req, res) => res.sendFile(INDEX) )
+  .post('/tables.create', (req, res) => {
+    // 新しい卓IDを生成
+    let tableId = randomstring.generate({
+        length: 8
+      , readable: true
+    });
+
+    // 卓を追加
+    RedisClient.HSET('tables', tableId, JSON.stringify({created: new Date().toJSON()}));
+
+    // 卓にアクセスするためのURLを生成
+    let urlBase = req.protocol + '://' + req.hostname;
+    let p1Url = `${urlBase}/t/${tableId}/p1`;
+    let p2Url = `${urlBase}/t/${tableId}/p2`;
+    let watchUrl = `${urlBase}/t/${tableId}/watch`;
+
+    res.json({p1Url: p1Url, p2Url: p2Url, watchUrl: watchUrl});
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
@@ -20,9 +40,9 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => {
-  let count = RedisClient.INCR('counter', (error, n) => {
-    io.emit('time', n);
-  });
+// setInterval(() => {
+//   let count = RedisClient.INCR('counter', (error, n) => {
+//     io.emit('time', n);
+//   });
   
-}, 1000);
+// }, 1000);
