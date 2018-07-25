136a35df4850a4d3efea3a2400bd67f8fad3ea21
Dice <tetradice@gmail.com>
2018-07-25 23:36:01 +0900

    delete hyper folder

--- src/hyper/types.tsx
+++
@@ -1,8 +0,0 @@
-export type TimeStr = string;
-export type PlayerSide = 1 | 2;
-export type SerializableObject = {[key: string]: SerializableValue};
-export type SerializableValue = SerializablePrimaryValue | SerializableArray | SerializableObject;
-export type SerializableArray = Array<SerializablePrimaryValue> | Array<SerializableObject>;
-export type SerializablePrimaryValue = undefined | null | boolean | string | number;
-
-export type RegionName = 'distance' | 'dust' | 'p1-hands' | 'p2-hands'
--- src/hyper/utils.tsx
+++
@@ -1,6 +0,0 @@
-/** 指定回数の繰り返し */
-export function loop(count: number, action: (index: number) => void): void{
-    for(let i = 0; i < count; i++){
-        action.call(this, i);
-    }
-}
