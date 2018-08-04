7caad309e7d8b42907d63f83a08e7798a829c821
Dice <tetradice@gmail.com>
2018-08-05 08:49:00 +0900

    不要なコメントを削除

--- src/sakuraba/typings/base.d.ts
+++ src/sakuraba/typings/base.d.ts
@@ -8,6 +8,4 @@ type VigorValue = 0 | 1 | 2;
 declare module "*.css" {
     const classes: {[className: string]: string} // css-moduleの結果をstring型のobjectに
     export = classes
-    // import style from "./foo.css"で読み込みたいなら下記（後述）
-    // export default classes
-  }
+}
