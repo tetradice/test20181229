306931203da2fdd30d515143b096a456ee673c0c
Dice <tetradice@gmail.com>
2018-08-09 23:28:59 +0900

    baseUrl、pathsの設定を変更

--- src/sakuraba/components/MariganButton.tsx
+++ src/sakuraba/components/MariganButton.tsx
@@ -1,6 +1,6 @@
 import { h } from "hyperapp";
-import { ActionsType } from "../actions";
-import * as utils from "../utils";
+import { ActionsType } from "sakuraba/actions";
+import * as utils from "sakuraba/utils";
 
 /** 集中力 */
 export const MariganButton = (p: {left: number, top: number}) => (state: state.State, actions: ActionsType) => {
