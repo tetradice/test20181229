6631393797ba52f5181b44820cbd42c74e56f821
Dice <tetradice@gmail.com>
2018-11-04 01:55:26 +0900

    決闘前のメガミの表示位置とサイズを調整

--- src/sakuraba/apps/main/components/MegamiFace.tsx
+++ src/sakuraba/apps/main/components/MegamiFace.tsx
@@ -4,13 +4,13 @@ import { Megami } from "sakuraba";
 import { ZIndex } from "sakuraba/const";
 
 /** メガミ顔 */
-export const MegamiFace = (p: {megami: Megami, left: number, top: number}) => (state: state.State, actions: ActionsType) => {
+export const MegamiFace = (p: {megami: Megami, left: number, top: number, width: number, height: number}) => (state: state.State, actions: ActionsType) => {
     // DOMを返す
     let styles: Partial<CSSStyleDeclaration> = {
           left: `${p.left * state.zoom}px`
         , top: `${p.top * state.zoom}px`
-        , width: `${240 * 2 * state.zoom}px`
-        , height: `${80 * 2 * state.zoom}px`
+        , width: `${p.width * state.zoom}px`
+        , height: `${p.height * state.zoom}px`
         , position: 'absolute'
         , opacity: '0.1'
         , zIndex: `${ZIndex.MEGAMI_FACE}`
