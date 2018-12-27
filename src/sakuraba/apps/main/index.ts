28489d4e7b48699d6bdc69141156ee4a773dd5ee
Dice <tetradice@gmail.com>
2018-12-27 09:49:08 +0900

    hyperapp loggerをいったん廃止

--- src/sakuraba/apps/main/index.ts
+++ src/sakuraba/apps/main/index.ts
@@ -1,10 +1,10 @@
 import { app } from 'hyperapp';
-import { withLogger } from "@hyperapp/logger"
+// import { withLogger } from "@hyperapp/logger"
 
 import view from './view';
 import { actions, ActionsType } from './actions';
 
 export function run(state: state.State, container: Element | null): ActionsType {
-    let appFunction = (state.environment === 'development' ? withLogger(app) : app);
+    let appFunction = app;
     return appFunction(state, actions, view, container);
 }
